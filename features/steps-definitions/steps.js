d6cdb3bed2cd37d356015bed2cab06b46bea58fb
Deborah Atwiine <deborahatwiine75@gmail.com>
2022-06-15 01:07:32 +0300

    new changes that brought undefined

--- features/steps-definitions/steps.js
+++ features/steps-definitions/steps.js
@@ -3,22 +3,22 @@ const { expect } = require('chai');
 
 
 //Multiplication
-Given('I have entered {} into the calculator for multiplication', function (int1) {
+Given('I have entered {int} into the calculator for multiplication', function (int1) {
     console.log(int1);    
 }
 );
 
-Given('I have also entered {} into the calculator', function (int2) {
+Given('I have also entered {int} into the calculator for multiplication', function (int2) {
     console.log(int2);    
 }
 );
 
-When('I press {} * {} on the calculator', function (int1, int2) {
+When('I press {int} * {int} on the calculator', function (int1, int2) {
     product = int1 * int2;
 }
 );
 
-Then('The result should be {int} on the screen', function (result) {
+Then('The result should be {int} on the screen after multiplication', function (result) {
     expect(product).to.equal(result);
 }
 );
@@ -31,7 +31,7 @@ Given('I have entered {int} into the calculator for addition', function (int1) {
     }
 );
 
-Given('I have also entered {int} into the calculator', function (int2) {
+Given('I have also entered {int} into the calculator for addition', function (int2) {
     console.log(int2);   
 }
 );
@@ -41,7 +41,7 @@ When('I press {int} + {int} on the calculator', function (int1, int2) {
 }
 );
 
-Then('The result should be {int} on the screen', function (result) {
+Then('The result should be {int} on the screen after addition', function (result) {
     expect(sum).to.equal(result);
 }   
 );
@@ -53,7 +53,7 @@ Then('The result should be {int} on the screen', function (result) {
 }
 );
 
-Given('I have also entered {int} into the calculator', function (int2) {
+Given('I have also entered {int} into the calculator for subtraction', function (int2) {
     console.log(int2);   
 }
 );
@@ -63,29 +63,29 @@ When('I press {int} - {int} on the calculator', function (int2, int1) {
 }
 );
 
-Then('The result should be {int} on the screen', function (result) {
+Then('The result should be {int} on the screen after subtraction', function (result) {
     expect(difference).to.equal(result);
 }
 );
 
 
 //Division
-Given('I have entered {int} into the calculator to get a quotienr', function (int1) {
+Given('I have entered {int} into the calculator for division', function (int1) {
     console.log(int1);    
 }
 );
 
-Given('I have also entered {int} into the calculator', function (int2) {
+Given('I have also entered {int} into the calculator for division', function (int2) {
     console.log(int2);    
 }
 );
 
-When('I press {int} divide by {int} on the calculator', function (int1, int2) {
-    quotient = int1 / int2;
+When('I press {int} divide by {int} on the calculator', function (int2, int1) {
+    quotient = int2 / int1;
 }
 );
 
-Then('The result should be {int} on the screen ', function (result) {
+Then('The result should be {int} on the screen after division', function (result) {
     expect(quotient).to.equal(result);
 }
 );
