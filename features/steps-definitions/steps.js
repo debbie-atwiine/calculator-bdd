e5a2d8f558d080aff7409bd9b102ea5a95a73070
Deborah Atwiine <deborahatwiine75@gmail.com>
2022-06-15 01:29:03 +0300

    new wprking changes

--- features/steps-definitions/steps.js
+++ features/steps-definitions/steps.js
@@ -2,36 +2,36 @@ const {Given, When, Then} = require('@cucumber/cucumber');
 const { expect } = require('chai');
 
 
-//Multiplication
-Given('I have entered {int} into the calculator for multiplication', function (int1) {
+//Multiplying
+Given('I have entered the {} into the calculator', function (int1) {
     console.log(int1);    
 }
 );
 
-Given('I have also entered {int} into the calculator for multiplication', function (int2) {
+Given('I have entered {} into the calculator', function (int2) {
     console.log(int2);    
 }
 );
 
-When('I press {int} * {int} on the calculator', function (int1, int2) {
+When('I press {} * {} on the calculator', function (int1, int2) {
     product = int1 * int2;
 }
 );
 
-Then('The result should be {int} on the screen after multiplication', function (result) {
+Then('the result should be {int} on the screen', function (result) {
     expect(product).to.equal(result);
 }
 );
 
 
 
-//Addition
-Given('I have entered {int} into the calculator for addition', function (int1) {
+//Adding
+Given('I have entered {int} into the calculator to add', function (int1) {
     console.log(int1);
     }
 );
 
-Given('I have also entered {int} into the calculator for addition', function (int2) {
+Given('I have also entered {int} into the calculator', function (int2) {
     console.log(int2);   
 }
 );
@@ -41,52 +41,52 @@ When('I press {int} + {int} on the calculator', function (int1, int2) {
 }
 );
 
-Then('The result should be {int} on the screen after addition', function (result) {
+Then('The result should be {int} on the screen', function (result) {
     expect(sum).to.equal(result);
 }   
 );
 
 
-//Subtraction
- Given('I have entered {int} into the calculator for subtraction', function (int1) {
+// //Subtracting
+ Given('I have entered {int} into the calculator to subtract', function (int1) {
     console.log(int1);   
 }
 );
 
-Given('I have also entered {int} into the calculator for subtraction', function (int2) {
-    console.log(int2);   
-}
-);
+    Given('I have also entered second {int} into the calculator', function (int2) {
+        console.log(int2);   
+    }
+    );
 
-When('I press {int} - {int} on the calculator', function (int2, int1) {
-    difference = int2 - int1;
-}
-);
+    When('I press {int} - {int} on the calculator', function (int2, int1) {
+        difference = int2 - int1;
+    }
+    );
 
-Then('The result should be {int} on the screen after subtraction', function (result) {
-    expect(difference).to.equal(result);
-}
-);
+    Then('The result should be {int} on the screen after subtracting', function (result) {
+        expect(difference).to.equal(result);
+    }
+    );
 
 
-//Division
-Given('I have entered {int} into the calculator for division', function (int1) {
-    console.log(int1);    
-}
-);
+    // division
+    Given('I have entered {int} into the calculator to get average', function (int1) {
+        console.log(int1);    
+    }
+    );
 
-Given('I have also entered {int} into the calculator for division', function (int2) {
-    console.log(int2);    
-}
-);
+        Given('I have also entered {int} into the calculator to divide', function (int2) {
+            console.log(int2);    
+        }
+        );
 
-When('I press {int} divide by {int} on the calculator', function (int2, int1) {
-    quotient = int2 / int1;
-}
-);
+        When('I press {int} divide {int} on the calculator', function (int1, int2) {
+            quotient = int1 / int2;
+        }
+        );
 
-Then('The result should be {int} on the screen after division', function (result) {
-    expect(quotient).to.equal(result);
-}
-);
+        Then('The result should be {int} on the screen after dividing', function (result) {
+            expect(quotient).to.equal(result);
+        }
+        );
 
--- features/test.feature
+++ features/test.feature
@@ -2,28 +2,28 @@ Feature: Calculator
 
 
      Scenario: Multiply two numbers
-     Given I have entered 50 into the calculator for multiplication
-        And I have also entered 70 into the calculator
+     Given I have entered 50 into the calculator to add
+        And I have entered 70 into the calculator
         When I press 50 * 70 on the calculator
-        Then The result should be 3500 on the screen
+        Then the result should be 3500 on the screen
         
 
     Scenario: Add two numbers
-        Given I have entered 10 into the calculator for addition
+        Given I have entered 10 into the calculator
         And I have also entered 20 into the calculator
         When I press 10 + 20 on the calculator
         Then The result should be 30 on the screen
     
         
     Scenario: Subtract two numbers
-        Given I have entered 10 into the calculator for subtraction
+        Given I have entered 10 into the calculator to subtract
         And I have also entered 20 into the calculator
         When I press 20 - 10 on the calculator
-        Then The result should be 10 on the screen
+        Then The result should be 10 on the screen after subtracting
 
 
     Scenario: Divide two numbers
-        Given I have entered 10 into the calculator to get a quotient
-        And I have also entered 20 into the calculator
-        When I press 20 divide by 10 on the calculator
-        Then The result should be 2 on the screen 
+        Given I have entered 10 into the calculator to get average
+        And I have also entered 20 into the calculator to divide
+        When I press 20 divide 10 on the calculator
+        Then The result should be 2 on the screen after dividing
